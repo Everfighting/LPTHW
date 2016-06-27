184aebd2ea087ee12112ec4d7a1c788ef6034bff
Everfighting <cbb903601682@163.com>
2016-06-27 14:16:49 +0800

    change the chapter

---
+++ ex34.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+#!/usr/bin/python
+animals = ['bear', 'python', 'peacock', 'hangaroo', 'whale', 'platypus']
+print "the 1st animal is at o and is animals[0]"
+print  "the 2nd is at 1 and is", animals[1]
+print  "the 3rd is at 2 and is", animals[2]
+print  "the 4th is at 3 and is", animals[3]
+print  "the 5th is at 4 and is", animals[4]
+print  "the 6th is at 5 and is", animals[5]
