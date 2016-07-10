f2ddfe77bdbf861a515b55190d39a8fa33698bd4
Everfighting <cbb903601682@163.com>
2016-07-10 12:33:21 +0800

    更复杂的用户输入

---
+++ ex48/skeleton/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
+
+config = {
+         'description': 'My Project',
+         'author': 'caibinbin',
+         'url': 'URL to get it at.',
+         'download_url': 'Where to download it.',
+         'author_email': 'My email.',
+         'version': '0.1',
+         'install_requirse': ['nose'],
+         'packages': ['NAME'],
+         'scripts': [],
+         'name': 'projectname'
+}
+
+setup(**config)
+    
+setup(**config)
